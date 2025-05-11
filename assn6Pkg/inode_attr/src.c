916e182fcc545567223f52ab74b9534a5e2e4d51
HartmannPsi <hartmann_psi@sjtu.edu.cn>
2025-05-11 16:46:59 +0800

    minor

--- assn6Pkg/inode_attr/src.c
+++ assn6Pkg/inode_attr/src.c
@@ -1,3 +1,6 @@
+// 读写文件扩展属性
+
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,28 +8,40 @@
 #include <sys/xattr.h>
 
 int main(int argc, char *argv[]) {
-  if (argc < 3) {
+  // 检查参数个数
+  if (argc < 4 || argc > 5) {
     printf("Usage: %s <get|set|del> <filename> <attr_name> [value]\n", argv[0]);
     return 1;
   }
 
-  const char *op = argv[1];
-  const char *file = argv[2];
+  const char *op = argv[1];    // 操作类型
+  const char *path = argv[2];  // 操作路径
+  const char *name = argv[3];  // 文件名
 
   if (strcmp(op, "get") == 0 && argc == 4) {
     char value[1024] = {0};
-    ssize_t len = getxattr(file, argv[3], value, sizeof(value));
-    if (len == -1)
+    ssize_t len = getxattr(path, name, value, sizeof(value));
+    if (len == -1) {
       perror("getxattr");
-    else
+      return 1;
+    } else
       printf("Value: %s\n", value);
+
   } else if (strcmp(op, "set") == 0 && argc == 5) {
-    if (setxattr(file, argv[3], argv[4], strlen(argv[4]), 0) == -1)
+    if (setxattr(path, name, argv[4], strlen(argv[4]), 0) == -1) {
       perror("setxattr");
+      return 1;
+    }
+
   } else if (strcmp(op, "del") == 0 && argc == 4) {
-    if (removexattr(file, argv[3]) == -1) perror("removexattr");
+    if (removexattr(path, name) == -1) {
+      perror("removexattr");
+      return 1;
+    }
+
   } else {
     printf("Invalid usage\n");
+    return 1;
   }
 
   return 0;
